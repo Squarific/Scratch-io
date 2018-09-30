2c2294154dae8c374eb5523d431952f852b61c77
Filip Smets <filipsmetsbelgium@gmail.com>
2018-09-30 16:17:59 +0200

    Update client.js

--- source/client/client.js
+++ source/client/client.js
@@ -32,7 +32,7 @@ http.createServer(function(request, response) {
 	}
 
 	// Scratch client wants to listen to a new type of event
-	if (params[0] == "register") {
+	if (socket && params[0] == "register") {
 		socket.on(params[1], function (event, data) {
 			if (typeof data !== "object" && data.length !== "number") {
 				throw "You can only send arrays of strings and integers";
